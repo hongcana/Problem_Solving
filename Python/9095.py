d426caaff613f5cdbb9148c9aa72948158d24620
hongcana <hongcana@kakao.com>
2023-03-11 18:19:12 +0900

    [DP] 9095

---
+++ Python/9095.py
@@ -0,0 +1,11 @@
+import sys
+read = sys.stdin.readline
+T = int(read())
+d = [0] * 11
+d[1] = 1
+d[2] = 2
+d[3] = 4
+for i in range(4, 11):
+    d[i] = d[i-1] + d[i-2] + d[i-3]
+for _ in range(T):
+    print(d[int(read().rstrip())])
