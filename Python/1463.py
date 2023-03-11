c8d28c0d42a4a2c8eaa62f7cf5e04ecde69cc540
hongcana <hongcana@kakao.com>
2023-03-11 17:45:26 +0900

    [DP] 1463

--- Python/1463.py
+++ Python/1463.py
@@ -1,18 +1,12 @@
 # bottom-up DP
 import sys
 read = sys.stdin.readline
-d = [0] * 1000001
-d[2] = 1
-d[3] = 1
-
-for i in range(4, 1000001):
-    if i % 3 == 0 and i % 2 == 0:
-        d[i] = min(1 + d[i//3], 1 + d[i//2], 1 + d[i-1])
-    elif i % 3 == 0:
-        d[i] = min(1 + d[i//3], 1 + d[i-1])
-    elif i % 2 == 0:
-        d[i] = min(1 + d[i//2], 1 + d[i-1])
-    else:
-        d[i] = 1 + d[i-1]
-
-print(d[int(read().rstrip())])
+x = int(read().rstrip())
+d = [0] * (x+1)
+for i in range(2, x+1):
+    d[i] = d[i-1] + 1
+    if i % 3 == 0:
+        d[i] = min(d[i//3] + 1, d[i])
+    if i % 2 == 0:
+        d[i] = min(d[i//2] + 1, d[i])
+print(d[x])
