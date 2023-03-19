f6b9228be871c2e3b54f041b265f9e3c8b01c5bd
hongcana <hongcana@kakao.com>
2023-03-19 17:58:12 +0900

    [Two Pointer] 2470

---
+++ Python/2470.py
@@ -0,0 +1,28 @@
+import sys
+read = sys.stdin.readline
+N = int(read().rstrip())
+li = list(map(int, read().split()))
+li.sort()
+start = 0
+end = N-1
+
+# 용액이 음수면 start를 +1한다.
+# 용액이 양수면 end를 -1한다.
+# 그러면서 최소 결과값을 갱신한다.
+# O(N)을 만족할 것이다.
+result = li[start] + li[end]
+pos = [li[start], li[end]]
+while start < end:
+    tmp = li[start] + li[end]
+    if abs(result) > abs(tmp):
+        result = tmp
+        pos = [li[start], li[end]]
+    if tmp == 0:
+        pos = [li[start], li[end]]
+        break
+    elif tmp > 0:
+        end -= 1
+    else:
+        start += 1
+
+print(*pos)
