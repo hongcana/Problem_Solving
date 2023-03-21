3401e498c928f5a51d938c3bd7764633fca512c9
hongcana <hongcana@kakao.com>
2023-03-21 16:09:43 +0900

    [Bisect 이용한 BS] 19637

--- Python/19637.py
+++ Python/19637.py
@@ -1,36 +1,15 @@
 import sys
+import bisect
 read = sys.stdin.readline
 N, M = map(int, read().split())
 li = []
-
-
-def bsearch(t, start, end):
-    if end < 0:
-        return 0
-    elif start > end:
-        return start
-
-    mid = (end + start) // 2
-    if li[mid][1] == t:
-        return mid
-    else:
-        if li[mid][1] > t:
-            result = bsearch(t, start, mid-1)
-        elif li[mid][1] < t:
-            result = bsearch(t, mid+1, end)
-    return result
-
-
-plist = {}
+pwr = []
 for _ in range(N):
     name, point = map(str, read().split())
-    if point in plist:
-        continue
-    else:
-        plist[point] = name
-        li.append([name, int(point)])
+    li.append(name)
+    pwr.append(int(point))
 
 for t in range(M):
     t = int(read().rstrip())
-    result = bsearch(t, 0, len(li)-1)
-    print(li[result][0])
+    result = bisect.bisect_left(pwr, t)
+    print(li[result])
